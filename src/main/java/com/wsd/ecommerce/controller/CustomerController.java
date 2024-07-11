f91dd0d6bd73ce8e89522cb9c026abbd6cbe76af
saiful.islam <mdsaiful.islam@reddotdigitalit.com>
2024-07-11 14:11:16 +0600

    change: use autowired

--- src/main/java/com/wsd/ecommerce/controller/CustomerController.java
+++ src/main/java/com/wsd/ecommerce/controller/CustomerController.java
@@ -1,23 +1,19 @@
 package com.wsd.ecommerce.controller;
 
 import com.wsd.ecommerce.dto.CustomerWishlistDTO;
-import com.wsd.ecommerce.model.Wishlist;
 import com.wsd.ecommerce.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
 
 @RestController
 public class CustomerController {
 
-    private final CustomerService customerService;
-
-    public CustomerController(CustomerService customerService) {
-        this.customerService = customerService;
-    }
+    @Autowired
+    private CustomerService customerService;
 
     @GetMapping("/customers/{customerId}/wishlist")
     public ResponseEntity<CustomerWishlistDTO> getWishlist(@PathVariable Long customerId) {
